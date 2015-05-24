ab817f10ab9dfca3fc45558e82afcfdf4798e421
Rachel Maher <rachelelizabethmaher@gmail.com>
2015-05-24 16:20:37 -0600

    search functionality working

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 angular.module('InstagramApp', [])
 
 .controller('MyController', function($scope, $http, $sce) {
-	$scope.embedUrl = "https://api.instagram.com";
-
-	$scope.trustSrc = function(src) {
-		return $sce.trustAsResourceUrl(src);
-	};
-
 	$scope.searchInstagram = function(tag) {
-		$scope.tag = tag;
-
-		var url = "https://api.instagram.com/v1/tags/{tag}/media/recent";
-		var request = {
-			callback: 'JSON_CALLBACK',
-			client_id: '48fb6fd85c754109b1c1e4ccc6e3aca0',
-			count: 20
-		};
+      	var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent";
 
 		$http({
 			method: 'JSONP',
 			url: url,
-			params: request
+			params: {
+              callback: 'JSON_CALLBACK',
+              client_id: '48fb6fd85c754109b1c1e4ccc6e3aca0',
+              count: 24
+			}
 		})
-		.success(function(data) {
-			$scope.results = data;
+		.success(function(response) {
+          	//console.log(response);
+			$scope.results = response.data;
 		})
 		.error(function() {
 			alert('error');
